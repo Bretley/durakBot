e52efc8d12a802667ce079381b51b9c0589bbe05
Frank Pasqualini <FJP11@pitt.edu>
2019-12-15 15:58:46 -0500

    added docstrings to player.py

--- player.py
+++ player.py
@@ -1,14 +1,59 @@
+"""
+A module used to store classes related the the representation of a player
+
+Classes
+-------
+Player
+    The representation of a player
+"""
+
+
 class Player:
+    """
+    A class used to represent a Player
+
+    Attributes
+    ----------
+    hand : list
+        The list of cards in the player's hand
+    num : int
+        The Player's ID
+
+    Methods
+    -------
+    take(card_list)
+        Adds cards to the player's hand
+    """
+
     def __init__(self, num):
+        """
+        Parameters
+        ----------
+        num : int
+            The Player's ID
+        """
         self.hand = []
         self.num = num
 
-    def take(self, cards):
-        for card in cards:
+    def take(self, card_list):
+        """
+        Adds a card to the player's hand
+
+        Parameters
+        ----------
+        card_list : list
+            The list of cards to add to the player's hand
+        """
+
+        for card in card_list:
             if card is not None:
                 self.hand.append(card)
 
     def __str__(self):
+        """
+        Returns the player as a string
+        """
+
         ret = "==" + str(self.num) + "==\n"
         ret += "hand:\n"
         for card in self.hand:
