04c97c505433763148e494bde1403683f52de65f
Frank Pasqualini <FJP11@pitt.edu>
2019-12-15 16:13:59 -0500

    added docstrings and temporary methods to game.py plus fixed some linting

--- game.py
+++ game.py
@@ -1,31 +1,84 @@
+"""
+A module used to store classes related the the representation of a game
+
+Classes
+-------
+Player
+    The representation of a game
+"""
+
 from deck import Deck
 from player import Player
 
 
 class Game:
-    def __init__(self, numPlayers):
-        """ visual:
+    """
+    A class used to represent a game
+
+    Attributes
+    ----------
+    players : list
+        The list of players
+    table_card : Card
+        The card on the table
+    dank: Card.suit
+        The suit of the trump card
+
+    Methods
+    -------
+    temp_public_method()
+        A method to be implemented later TODO
+    temp_public_method_2()
+        A method to be implemented later TODO
+    """
+    def __init__(self, num_players):
+        """
+        visual:
             2
         1       3
             0
-        """
-        """
-            Deal 6 to each
-            Determine who has initial lowest dank (otherwise default to 0)
-        """
-        d = Deck()
-        d.shuffle()
-        self.players = [Player(x) for x in range(numPlayers)]
-        for cardNum in range(6):
-            for p in self.players:
-                p.take(d.draw())
 
-        self.tableCard = d.flip()
-        self.dank = self.tableCard.suit  # Determine Dank suit
+        Deal 6 to each
+        Determine who has initial lowest dank (otherwise default to 0)
+
+        Parameters
+        ----------
+        num_players : int
+            The number of players
+        """
+        deck = Deck()
+        deck.shuffle()
+
+        self.players = [Player(x) for x in range(num_players)]
+        for _ in range(6):
+            for player in self.players:
+                player.take(deck.draw())
+
+        self.table_card = deck.flip()
+        self.dank = self.table_card.suit  # Determine Dank suit
         print('Table Card:')
-        print(self.tableCard)
-        for p in self.players:
-            print(p)
+        print(self.table_card)
+        for player in self.players:
+            print(player)
+
+    def temp_public_method(self):
+        """
+        A method to be implemented later TODO
+        """
+
+    def temp_public_method_2(self):
+        """
+        A method to be implemented later TODO
+        """
 
 
-g = Game(4)
+def main():
+    """
+    The main function for the game
+    """
+    game = Game(4)
+    del game  # For now until we use something else with the game variable
+
+
+if __name__ == "__main__":
+    main()
