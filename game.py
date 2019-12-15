577930452fed40f91099c34f9197a612da003347
Bret <bbarkley@psc.edu>
2019-12-15 15:39:25 -0500

    initial setup

---
+++ game.py
@@ -0,0 +1,31 @@
+from deck import *
+from player import Player
+class Game:
+    def __init__(self, numPlayers):
+        """ visual:
+            2
+        1       3
+            0
+        """
+        """
+            Deal 6 to each
+            Determine who has initial lowest dank (otherwise default to 0)
+        """
+        d = Deck()
+        d.shuffle()
+        self.players = [Player(x) for x in range(numPlayers)]
+        for cardNum in range(6):
+            for p in self.players:
+                p.take(d.draw())
+
+        self.tableCard = d.flip()
+        self.dank = self.tableCard.suit # Determine Dank suit
+        print( 'Table Card:')
+        print( self.tableCard)
+        for p in self.players:
+            print( p)
+
+g = Game(4)
+
+
+
