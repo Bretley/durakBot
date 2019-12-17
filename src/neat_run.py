2e43d6218ae2f078dc7914f63a3cde3090394f08
Frank Pasqualini <FJP11@pitt.edu>
2019-12-17 00:03:53 -0500

    Modified neat_run.py to something that is hopefully usable once the DurakEnv is completed

--- src/neat_run.py
+++ src/neat_run.py
@@ -1,58 +1,106 @@
-import gym
-import neat
+"""
+TODO
+"""
+
+import argparse
 import os
+
+import neat
 
 from src.durak_env import DurakEnv
 
 
 class Worker:
+    """
+    TODO
+    """
+
     def __init__(self, genome, config):
+        """
+        TODO
+        """
+
         self.genome = genome
         self.config = config
         self.env = None
 
     def work(self):
+        """
+        TODO
+        """
+
+        # Loads in a default Durak state
         self.env = DurakEnv()
         self.env.reset()
 
+        net = neat.nn.FeedForwardNetwork.create(self.genome, self.config)
+
+        # Takes the first step to get an observation ofn the current state
+        observation, _, _, _ = self.env.step(self.env.action_space.sample())
+        reward = 0
         done = False
+        info = None
 
+        # Loops through the game until the game is finished or the machine makes an unforgivable mistake
         while not done:
-            actions = None
-            _, _, done, _ = self.env.step(actions)
+            actions = net.activate(observation)
+            observation, reward, done, info = self.env.step(actions)
 
-        fitness = 0
-        return fitness
+        del info
+
+        return reward
 
 
 def eval_genomes(genome, config):
+    """
+    TODO
+    """
+
+    # Use the worker class for simplicity
     worker = Worker(genome, config)
     return worker.work()
 
 
 def main(config_file, restore_file):
+    """
+    TODO
+    """
+
     # Load configuration.
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_file)
 
+    # Load Restore file if it is specified
     if restore_file is None:
-        p = neat.Population(config)
+        population = neat.Population(config)
     else:
-        p = neat.Checkpointer.restore_checkpoint(restore_file)
+        population = neat.Checkpointer.restore_checkpoint(restore_file)
 
-    p.add_reporter(neat.StdOutReporter(True))
+    # Create reporters
+    population.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
-    p.add_reporter(stats)
-    p.add_reporter(neat.Checkpointer(generation_interval=10, filename_prefix='../restores/neat-checkpoint-'))
+    population.add_reporter(stats)
+    population.add_reporter(neat.Checkpointer(generation_interval=10, filename_prefix='../restores/neat-checkpoint-'))
 
-    pe = neat.ParallelEvaluator(10, eval_genomes)
-
-    winner = p.run(pe.evaluate)
+    # Run the learning in parallel
+    evaluator = neat.ParallelEvaluator(10, eval_genomes)
+    winner = population.run(evaluator.evaluate)
 
     print(winner)
 
 
 if __name__ == '__main__':
-    local_dir = os.path.dirname(__file__)
-    config_path = os.path.normpath(os.path.join(local_dir, '..', 'config', '.NEAT'))
-    restore_path = None  # os.path.normpath(os.path.join(local_dir, '..', 'restores', 'neat-checkpoint-1'))
-    main(config_path, restore_path)
+    # Reads in optional file specification arguments
+    PARSER = argparse.ArgumentParser(description="Run NEAT on the Durak game.")
+    PARSER.add_argument('--config', type=str, default="../config/.NEAT", required=False)
+    PARSER.add_argument('--restore', type=str, default=None, required=False)
+    ARGS = PARSER.parse_args()
+
+    LOCAL_DIR = os.path.dirname(__file__)
+    CONFIG_PATH = os.path.normpath(os.path.join(LOCAL_DIR, ARGS.config))
+
+    if ARGS.restore is None:
+        RESTORE_PATH = None
+    else:
+        RESTORE_PATH = os.path.normpath(os.path.join(LOCAL_DIR, ARGS.restore))
+
+    main(CONFIG_PATH, RESTORE_PATH)
