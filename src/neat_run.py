9f10a468e5a13dff2cc064115f093211218aa2f2
Frank Pasqualini <FJP11@pitt.edu>
2019-12-16 22:56:22 -0500

    Beginning of NEAR implementation

---
+++ src/neat_run.py
@@ -0,0 +1,50 @@
+import gym
+import neat
+import os
+
+
+class Worker:
+    def __init__(self, genome, config):
+        self.genome = genome
+        self.config = config
+        self.env = None
+
+    def work(self):
+        self.env = gym.make("Durak")
+        self.env.reset()
+
+        fitness = 0
+        return fitness
+
+
+def eval_genomes(genome, config):
+    worker = Worker(genome, config)
+    return worker.work()
+
+
+def main(config_file, restore_file):
+    # Load configuration.
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_file)
+
+    if restore_file is None:
+        p = neat.Population(config)
+    else:
+        p = neat.Checkpointer.restore_checkpoint(restore_file)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(generation_interval=10, filename_prefix='../restores/neat-checkpoint-'))
+
+    pe = neat.ParallelEvaluator(10, eval_genomes)
+
+    winner = p.run(pe.evaluate)
+
+    print(winner)
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.normpath(os.path.join(local_dir, '..', 'config', '.NEAT'))
+    restore_path = None  # os.path.normpath(os.path.join(local_dir, '..', 'restores', 'neat-checkpoint-1'))
+    main(config_path, restore_path)
