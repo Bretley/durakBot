83e535c3acb7f9fbaacb77dd2699f664c0c9f810
Frank Pasqualini <FJP11@pitt.edu>
2019-12-17 16:24:15 -0500

    cleaned up Player.py

---
+++ doc/player.md
@@ -0,0 +1,73 @@
+Module player
+=============
+A module used to store classes related the the representation of a player.
+
+Classes
+-------
+
+`Player(num, strategy)`
+:   A class used to represent a Player (simple bot).
+    
+    Attributes:
+        hand: The list of cards in the player's hand.
+        num : The Player's ID.
+        dank: The suit of the Dank card.
+        strategy: The strategy the bot uses.
+    
+    Inits Player with an ID and strategy.
+    
+    Args:
+        num: The Player's ID
+        strategy: Class must implement shed, attack, and defend for the bot.
+
+    ### Methods
+
+    `attack(self, table, ranks)`
+    :   Does an attack action.
+        
+        Args:
+            table : The cards on the table.
+            ranks: TODO(Bretley)
+        
+        Returns:
+            The return of the strategy's attack.
+
+    `defend(self, table, pass_is_legal, cards_to_defend)`
+    :   Does a defense action.
+        
+        Args:
+            table: The cards on the table.
+            pass_is_legal: Whether or not a pass is legal.
+            cards_to_defend: The number of cards to defend against.
+        
+            Returns:
+                The return of the strategy's defend.
+
+    `shed(self, table, max_shed_allowed, ranks)`
+    :   Sheds cards to the player's hand
+        
+        Args:
+            table: The list of cards on the table.
+            max_shed_allowed: The maximum amount of cards to shed.
+            ranks: TODO(Bretley)
+        
+            Returns:
+                The return of the strategy's shed.
+
+    `sort(self)`
+    :   Sorts the player's hand
+
+    `take(self, card)`
+    :   Adds card to the player's hand.
+        
+        Args:
+            card: The card to add to the hand.
+
+    `take_table(self, cards)`
+    :   Adds cards to the player's hand
+        
+        Args:
+            cards: The list of cards to add to the player's hand.
+
+    `verify_hand(self)`
+    :   Method to ensure that the hand contains 0 duplicates
--- src/player.py
+++ src/player.py
@@ -1,88 +1,36 @@
-"""
-A module used to store classes related the the representation of a player
-
-Classes
--------
-Player
-    The representation of a player
-Attack
-    Enum for attacking
-Defense
-    Enum for defending
+"""A module used to store classes related the the representation of a player.
 """
 
 from card import CARD_COMPARATORS
 
-from strategy import S0, S1
-
 
 class Player:
-    """
-    A class used to represent a Player (simple bot)
+    """A class used to represent a Player (simple bot).
 
-    Attributes
-    ----------
-    hand : list(Card)
-        The list of cards in the player's hand
-    num : int
-        The Player's ID
-    dank : Card.suit
-        The suit of the Dank card
-
-    strategy:
-        class that implements attack, defend, and shed methods
-
-    Methods
-    -------
-    attack(table)
-        Does an attack action
-    defend(table, pass_is_legal, cards_to_defend)
-        Does a defense action
-    sort()
-        Sort's the player's hand
-    take(card)
-        Adds card to the player's hand
-    take_table(cards)
-        Adds cards to the player's hand
-    shed(table, max_shed_allowed)
-        Sheds cards to the player's hand
+    Attributes:
+        hand: The list of cards in the player's hand.
+        num : The Player's ID.
+        dank: The suit of the Dank card.
+        strategy: The strategy the bot uses.
     """
 
     def __init__(self, num, strategy):
+        """Inits Player with an ID and strategy.
+
+        Args:
+            num: The Player's ID
+            strategy: Class must implement shed, attack, and defend for the bot.
         """
-        Parameters
-        ----------
-        num :
-            The Player's ID
-        strategy: Class
-            class must implement shed, attack, and defend for the bot
-        """
+
         self.hand = []
         self.num = num
         self.dank = None
         self.strategy = strategy
 
     def __len__(self):
-        """
-        Implements len function for Player
-
-        Returns
-        -------
-        int
-            The number of cards in the hand
-        """
         return len(self.hand)
 
     def __str__(self):
-        """
-        Implements str function for Player
-
-        Returns
-        -------
-        str
-            The player as a string
-        """
-
         ret = "==" + str(self.num) + "==\n"
         ret += "hand:\n"
         for card in self.hand:
@@ -90,97 +38,74 @@ class Player:
         return ret
 
     def verify_hand(self):
-        """
-        Method to ensure that the hand contains 0 duplicates
+        """Method to ensure that the hand contains 0 duplicates
         """
         return len(self.hand) == len(set(self.hand))
 
     def attack(self, table, ranks):
-        return self.strategy.attack(self.hand, table, self.dank, ranks)
-        """
-        Does an attack action
-        ----------
-        table : Table
-            The cards on the table
+        """Does an attack action.
 
-        Returns
-        -------
-        Attack, Card
-            An Attack enum, the card to be removed
+        Args:
+            table : The cards on the table.
+            ranks: TODO(Bretley)
+
+        Returns:
+            The return of the strategy's attack.
         """
+        return self.strategy.attack(self.hand, table, self.dank, ranks)
 
     def defend(self, table, pass_is_legal, cards_to_defend):
+        """Does a defense action.
+
+        Args:
+            table: The cards on the table.
+            pass_is_legal: Whether or not a pass is legal.
+            cards_to_defend: The number of cards to defend against.
+
+            Returns:
+                The return of the strategy's defend.
+        """
         return self.strategy.defend(self.hand, table, self.dank, pass_is_legal, cards_to_defend)
-        """
-        Does a defense action
-
-        Parameters
-        ----------
-        table : list(Card)
-            The cards on the table
-        pass_is_legal : bool
-            Whether or not a pass is legal
-        cards_to_defend : int
-            The number of cards to defend against
-
-        Returns
-        -------
-        Defense, list
-            A Defense enum, a list of Cards
-        """
 
     def sort(self):
-        """
-        Sort's the player's hand
+        """Sorts the player's hand
         """
 
         self.hand.sort(key=CARD_COMPARATORS[self.dank])
 
     def take(self, card):
-        """
-        Adds card to the player's hand
+        """Adds card to the player's hand.
 
-        Parameters
-        ----------
-        card : Card
-            The card to add to the hand
+        Args:
+            card: The card to add to the hand.
         """
 
         if card is not None:
             self.hand.append(card)
 
         if self.dank is not None:
-            # This might be more efficient than constantly iterating over
-            # hands over and over to find least valuable card
+            # This might be more efficient than constantly iterating over hands over and over to find least valuable card.
             self.sort()
 
     def take_table(self, cards):
-        """
-        Adds cards to the player's hand
+        """Adds cards to the player's hand
 
-        Parameters
-        ----------
-        cards : list(Card)
-            The list of cards to add to the player's hand
+        Args:
+            cards: The list of cards to add to the player's hand.
         """
 
         self.hand += cards
         self.sort()
 
     def shed(self, table, max_shed_allowed, ranks):
+        """Sheds cards to the player's hand
+
+        Args:
+            table: The list of cards on the table.
+            max_shed_allowed: The maximum amount of cards to shed.
+            ranks: TODO(Bretley)
+
+            Returns:
+                The return of the strategy's shed.
+            """
         return self.strategy.shed(self.hand, table, self.dank, max_shed_allowed, ranks)
-        """
-        Sheds cards to the player's hand
-
-        Parameters
-        ----------
-        table : list(Card)
-            The list of cards on the table
-        max_shed_allowed : int
-            The maximum amount of cards to shed
-
-        Returns
-        -------
-        list
-            A list of Cards
-        """
